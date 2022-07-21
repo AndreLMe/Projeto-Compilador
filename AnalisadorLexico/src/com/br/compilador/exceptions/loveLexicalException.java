2e666612eaf121ed90cd5a2cc67ec21903f33dcd
andre <andre_luiz@protonmail.com>
2022-07-21 00:01:13 -0300

    Remocao do antigo leitor

--- AnalisadorLexico/src/com/br/compilador/exceptions/loveLexicalException.java
+++
@@ -1,7 +0,0 @@
-package AnalisadorLexico.src.com.br.compilador.exceptions;
-
-public class loveLexicalException extends RuntimeException{
-    public loveLexicalException(String ex){
-        super(ex);
-    }
-}
