75109eb402af848bdfee9c6a749cf23e185928ad
AndreLMe <andre_luiz@protonmail.com>
2022-08-24 21:37:54 -0300

    Novos testes

--- MainClass.java
+++ MainClass.java
@@ -4,24 +4,14 @@ public class MainClass{
        Scanner _key = new Scanner(System.in);
 double  a;
 double  b;
-double  c;
-double  d;
-double  f;
-String  t1;
-b = 2.0;
+b = 1;
+a = 1;
+System.out.println(a);
+System.out.println(b);
 a= _key.nextDouble();
 b= _key.nextDouble();
-t1 = "Ola Prof Isidro";
-System.out.println(t1);
-a = 1+2.0*3/b;
 if (a>b) {
 System.out.println(a);System.out.println(b);}else {
 System.out.println(b);}
 
-c = 5;
-d = 1;
-
-while(c>d){
-System.out.println(d);d = d+1;
-}
   }}
