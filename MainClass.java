86c802ce337f337dc42c57e8de9c8ae254984d4f
AndreLMe <andre_luiz@protonmail.com>
2022-08-24 21:16:48 -0300

    Novos exemplos

--- MainClass.java
+++ MainClass.java
@@ -11,12 +11,14 @@ String  t1;
 b = 2.0;
 a= _key.nextDouble();
 b= _key.nextDouble();
+t1 = "Ola Prof Isidro";
+System.out.println(t1);
 a = 1+2.0*3/b;
 if (a>b) {
 System.out.println(a);System.out.println(b);}else {
 System.out.println(b);}
 
-c = 3;
+c = 5;
 d = 1;
 
 while(c>d){
